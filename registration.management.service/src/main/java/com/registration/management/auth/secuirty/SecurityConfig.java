fe6d34876d097e438473f3db18ce45c1d1d6266e
ishmeet-codes <ishmeetsinghxa@gmail.com>
2026-08-30 15:59:18 +0530

    Cors url update

--- registration.management.service/src/main/java/com/registration/management/auth/secuirty/SecurityConfig.java
+++ registration.management.service/src/main/java/com/registration/management/auth/secuirty/SecurityConfig.java
@@ -65,7 +65,7 @@ public class SecurityConfig {
     @Bean
     public org.springframework.web.cors.CorsConfigurationSource corsConfigurationSource() {
         org.springframework.web.cors.CorsConfiguration configuration = new org.springframework.web.cors.CorsConfiguration();
-        configuration.setAllowedOrigins(java.util.List.of("http://localhost:5173", "http://localhost:3000")); // common frontend ports
+        configuration.setAllowedOrigins(java.util.List.of("http://localhost:5173", "http://localhost:3000", "https://registration-management-system-fron.vercel.app")); // common frontend ports
         configuration.setAllowedMethods(java.util.List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(java.util.List.of("*"));
         configuration.setAllowCredentials(true);
