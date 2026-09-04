66611d80c53358aa953f079f2004df4c7fd43c06
codebyjsa <jsaishere@gmail.com>
2026-09-05 01:19:09 +0530

    fix(test): remove deprecated MappingJackson2HttpMessageConverter from RegistrationControllerTest

--- registration.management.service/src/test/java/com/registration/management/registration/controller/RegistrationControllerTest.java
+++ registration.management.service/src/test/java/com/registration/management/registration/controller/RegistrationControllerTest.java
@@ -21,7 +21,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
@@ -55,7 +54,6 @@ class RegistrationControllerTest {
         objectMapper.registerModule(new JavaTimeModule());
 
         mockMvc = MockMvcBuilders.standaloneSetup(registrationController)
-                .setMessageConverters(new MappingJackson2HttpMessageConverter(objectMapper))
                 .setControllerAdvice(new GlobalExceptionHandler())
                 .build();
 
