bece0d2628fd28c56e4fd1abe4f95a37af19d148
ishmeet-codes <ishmeetsinghxa@gmail.com>
2026-09-10 20:14:15 +0530

    cron job added in frontend

--- registration.management.service/src/main/java/com/registration/management/auth/secuirty/SecurityConfig.java
+++ registration.management.service/src/main/java/com/registration/management/auth/secuirty/SecurityConfig.java
@@ -50,6 +50,8 @@ public class SecurityConfig {
                                 "/api/auth/refresh",
                                 "/api/auth/password/forgot",
                                 "/api/auth/password/reset",
+                                "/actuator/health",
+                                "/actuator/**",
                                 "/error"
                         ).permitAll()
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
